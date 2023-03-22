75ed17f1ddea1968195fdd9aced878e20b933e68
Evgeniy <golowin056@gmail.com>
2023-03-22 16:33:05 +0200

    create sort for id and name

--- src/App.js
+++ src/App.js
@@ -1,41 +1,108 @@
 import {useEffect, useState} from "react";
-import './App.css';
 import axios from "axios";
+import sortUp from './sort_ascending_icon.png';
+import sortDown from './sort_descending_icon.png'
+
+import './App.css';
 
 
 export default function App() {
-
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [url, setUrl] = useState('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}&limit=5')
+    const baseUrl = "http://www.filltext.com";
+    const url = `${baseUrl}/?rows=10&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+
+    const compareFirstNameAscending = (a, b) => {
+        if (a.firstName < b.firstName) {
+            return -1;
+        }
+        if (a.firstName > b.firstName) {
+            return 1;
+        }
+        return 0;
+    };
+
+    const sortByFirstNameAscending = () => {
+        const sortedData = [...data].sort(compareFirstNameAscending);
+        setData(sortedData);
+    };
+    const compareFirstNameDescending = (a, b) => {
+        if (a.firstName < b.firstName) {
+            return 1;
+        }
+        if (a.firstName > b.firstName) {
+            return -1;
+        }
+        return 0;
+    };
+
+    const sortByFirstNameDescending = () => {
+        const sortedData = [...data].sort(compareFirstNameDescending);
+        setData(sortedData);
+    };
+
+
+    const compareIdAscending = (a, b) => {
+        return a.id - b.id;
+    };
+    const sortByIdAscending = () => {
+        const sortedData = [...data].sort(compareIdAscending);
+        console.log(sortedData, '1-2')
+        setData(sortedData);
+    };
+
+    const compareIdDescending = (a, b) => {
+        return b.id - a.id;
+    };
+    const sortByIdDescending = () => {
+        const sortedData = [...data].sort(compareIdDescending);
+        console.log(sortedData, '2-1')
+        setData(sortedData);
+    };
+
 
     useEffect(() => {
         axios.get(url)
             .then(response => setData(response.data))
             .catch(error => console.log(error))
-        setData(state => {
-            state.sort((a, b) => a.id - b.id)
-            return state;
-        })
-        dataFunc();
-    }, [url]);
-    console.log(data)
-    const dataFunc = async () => {
-        setLoading(true)
-        const res = await axios.get(url)
-        setLoading(false)
-    }
+    }, []);
 
-    return (<div className="table">
-            {data.map(item => (<div className="item_table" key={item.id}>
-                <div loading={loading}>
-                    <p className="cell">{item.id}</p>
-                    <p className="cell">{item.firstName} {item.lastName}</p>
-                    <p className="cell">{item.email}</p>
-                    <p className="cell">{item.phone}</p>
-                    <p className="cell">{item.address.streetAddress} ,{item.address.city},{item.address.state},{item.address.zip}</p>
-                </div>
-            </div>))}
-        </div>
+    return (<table className="table">
+            <thead>
+            <tr>
+                <th
+                >Id <img onClick={sortByIdAscending} src={sortUp}/>
+                    <img onClick={sortByIdDescending} src={sortDown}/>
+                </th>
+                <th>FirstName <img onClick={sortByFirstNameAscending} src={sortUp}/><img
+                    onClick={sortByFirstNameDescending} src={sortDown}/></th>
+                <th>LastName</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+            </tr>
+            </thead>
+            <tbody>
+            {data.map((item) => (
+                <tr className="item_table" key={item.id + item.firstName}>
+                    <td className="cell">{item.id}</td>
+                    <td className="cell">{item.firstName}</td>
+                    <td className="cell">{item.lastName}</td>
+                    <td className="cell">{item.email}</td>
+                    <td className="cell">{item.phone}</td>
+                    <td className="cell">
+                        {item.address.streetAddress}, {item.address.city}, {item.address.state},{" "}
+                        {item.address.zip}
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+            {/*<input*/}
+            {/*    value={searchTerm}*/}
+            {/*    autoFocus*/}
+            {/*    type='text'*/}
+            {/*    placeholder='search'*/}
+            {/*    onChange={(e) => setSearchTerm(e.target.value)}*/}
+            {/*/>*/}
+        </table>
     );
 }
